78b68bddfcbb0ebefb3ae4c441543342dcb7e85c
kuloteyesda <kuloteyesda@gmail.com>
2025-07-15 00:33:36 +0700

    Delete src directory

--- src/utils/site.ts
+++
@@ -1,4 +0,0 @@
-export const nama = 'Bokep Pap Pacar';
-export const desk = 'Video Bokep Indo Bocil Ngentot Jilbab Smp Pacar Sma Pap Tante Tobrut Terbaru Viral';
-export const url = 'https://bokeppappacar.pages.dev';
-export const terbit = '2025-06-27T00:01:03Z';
